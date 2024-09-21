36ab90c5293c889d8d8460c8d2f9f821f10a66fc
N0elung <noelb.davalos@gmail.com>
2024-09-21 12:59:22 -0700

    Practica con Nav

---
+++ NavMobile.js
@@ -0,0 +1,8 @@
+function AbrirMenu(){
+    var x = document.getElementById("Opciones");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
